c3bd37eda3e9f27f5fff1314ba7fc4816c179bdd
BugHunter-X <askzenai@gmail.com>
2024-12-22 04:56:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Some Tailwind classes are not applied, or they're applied incorrectly.
+<div class="bg-red-500 p-4 text-white">
+  This text should be red, but it's not.
+</div>
+```
